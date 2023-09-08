4cb15ceda221f3c416b8644b2e83d3e766d1ba97
Ankit Kanaujia <ankit.rank1@gmail.com>
2023-09-08 22:19:22 +0530

    changes

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      // Add this custom launcher section
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
